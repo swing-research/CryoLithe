89f7b8c3c23b78f20b27c752cf1869aa5abe3ed8
vinith2 <vinithcolab@gmail.com>
2025-04-18 15:37:58 +0200

    ribosome update

--- download_ribosome.sh
+++ download_ribosome.sh
@@ -4,4 +4,5 @@ mkdir -p ./data/
 # Download the ribosome data
 wget https://drive.switch.ch/index.php/s/fWr8ZcFQiiU0OJv/download -O ribo_data.zip
 unzip ribo_data.zip -d ./data/
-
+# Remove the zip file
+rm ribo_data.zip
