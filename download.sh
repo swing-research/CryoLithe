a37b49e0db0d320d2ff1ab7f334cddfbf7e8b743
vinith2 <85550536+vinith2@users.noreply.github.com>
2025-10-14 16:11:42 +0200

    Update download link for trained models

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-wget https://drive.switch.ch/index.php/s/bLW0W3gqnwbV5qJ/download -O trained_models.zip
+wget https://drive.switch.ch/index.php/s/rVMAxUubCj3C6C7/download -O trained_models.zip
 unzip trained_models.zip -d trained_models
 rm trained_models.zip
