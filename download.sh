5842351749144fc4e92e4d2831ec817d51705a57
Vinith <85550536+vinith2@users.noreply.github.com>
2026-02-03 15:01:03 +0100

    Update download link for trained_models.zip
    with icecream reference trained models

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-wget https://drive.switch.ch/index.php/s/rVMAxUubCj3C6C7/download -O trained_models.zip
+wget https://drive.switch.ch/index.php/s/1dkw89VmLnJxSlU/download -O trained_models.zip
 unzip trained_models.zip -d trained_models
 rm trained_models.zip
